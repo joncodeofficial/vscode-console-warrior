548d0bac445e1721fcac10418995472d9ce45219
jonpena <gran_200014@hotmail.com>
2025-05-23 01:24:44 +0200

    fix: add space before ellipsis in truncateString function for consistency

--- src/utils/truncateString.ts
+++ src/utils/truncateString.ts
@@ -2,6 +2,6 @@ import { MAX_MESSAGE_LENGTH } from "../constants";
 
 export const truncateString = (str: string) => {
   return str.length > MAX_MESSAGE_LENGTH
-    ? str.slice(0, MAX_MESSAGE_LENGTH) + "..."
+    ? str.slice(0, MAX_MESSAGE_LENGTH) + " ..."
     : str;
 };
