6d50163313f663fa4256dbe52961b58a0ec45f76
jonpena <gran_200014@hotmail.com>
2025-05-23 01:24:51 +0200

    refactor: capitalize constants in checkIfNodeModulesReady function for consistency

--- src/utils/checkIfNodeModulesReady.ts
+++ src/utils/checkIfNodeModulesReady.ts
@@ -2,17 +2,17 @@ import fs from "fs";
 
 export const checkIfNodeModulesReady = async (
   dir: string,
-  maxTries = 20,
-  delay = 500
+  MAX_TRIES = 20,
+  DELAY = 500
 ): Promise<boolean> => {
-  for (let i = 0; i < maxTries; i++) {
+  for (let i = 0; i < MAX_TRIES; i++) {
     try {
       const items = fs.readdirSync(dir).filter((name) => !name.startsWith("."));
       if (items.length > 0) return true;
     } catch (e) {
       console.log(e);
     }
-    await new Promise((r) => setTimeout(r, delay));
+    await new Promise((r) => setTimeout(r, DELAY));
   }
   return false;
 };
