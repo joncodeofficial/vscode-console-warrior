cdda548e38af404e2eb4edc7448a04055c564ad5
jonpena <gran_200014@hotmail.com>
2025-05-24 20:43:17 +0200

    test: add unit tests for formatString function

---
+++ src/test/utils/formatString.test.ts
@@ -0,0 +1,20 @@
+import * as assert from "assert";
+import { formatString } from "../../utils/formatString";
+
+suite("formatString Tests", () => {
+  test("should remove literal newlines", () => {
+    assert.equal(formatString("Hello\nWorld"), "Hello\nWorld");
+  });
+
+  test("should remove literal tabs", () => {
+    assert.equal(formatString("Hello\tWorld"), "Hello\tWorld");
+  });
+
+  test("should trim spaces at start and end", () => {
+    assert.equal(formatString("   Hello World   "), "Hello World");
+  });
+
+  test("should replace multiple spaces with one", () => {
+    assert.equal(formatString("Hello    World"), "Hello World");
+  });
+});
