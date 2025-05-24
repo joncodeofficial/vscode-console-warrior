e6a16de01fa04d72bb58f179e30c21369475b0a5
jonpena <gran_200014@hotmail.com>
2025-05-24 20:14:11 +0200

    refactor: clean up formatString function by removing unnecessary newline and tab handling

--- src/utils/formatString.ts
+++ src/utils/formatString.ts
@@ -1,9 +1,5 @@
 export const formatString = (message: string) => {
   return message
-    .replace(/\n/g, "") // Eliminar saltos de línea literales
-    .replace(/\\n/g, "") // Eliminar \n como texto
-    .replace(/\t/g, "") // Eliminar tabs literales
-    .replace(/\\t/g, "") // Eliminar \t como texto
-    .replace(/^\s+|\s+$/g, "") // Eliminar espacios al inicio y final
-    .replace(/\s{2,}/g, " "); // Reemplazar múltiples espacios por uno solo
+    .replace(/^\s+|\s+$/g, "") // Delete spaces at start and end
+    .replace(/\s{2,}/g, " "); // Replace multiple spaces with one
 };
