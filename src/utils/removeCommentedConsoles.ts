ccbd0b8042985d08349bee490264dc5a6b44b285
jonpena <gran_200014@hotmail.com>
2025-06-08 01:04:09 +0200

    feat: add removeCommentedConsoles utility to manage console logs in editor

---
+++ src/utils/removeCommentedConsoles.ts
@@ -0,0 +1,23 @@
+import { IConsoleDataMap } from "../types/consoleDataMap.interface";
+import * as vscode from "vscode";
+import { isConsoleLogCorrect } from "./isConsoleLogCorrect";
+
+export const removeCommentedConsoles = (
+  editor: vscode.TextDocumentChangeEvent,
+  consoleDataMap: IConsoleDataMap
+) => {
+  const filePath = editor.document.uri.fsPath;
+
+  for (const [file, innerMap] of consoleDataMap) {
+    if (filePath.endsWith(file)) {
+      for (const [key] of innerMap) {
+        const lineText = editor.document.lineAt(parseInt(key) - 1).text;
+
+        if (!isConsoleLogCorrect(lineText) && innerMap.has(key)) {
+          innerMap.delete(key);
+        }
+      }
+      if (innerMap.size === 0) consoleDataMap.delete(file);
+    }
+  }
+};
