e8163b111ccf9098ca006d7fb24ff840b9acfb29
jonpena <gran_200014@hotmail.com>
2025-06-28 02:58:44 +0200

    refactor: simplify checkIfNodeModulesReady function by removing parameters

--- src/utils/checkIfNodeModulesReady.ts
+++ src/utils/checkIfNodeModulesReady.ts
@@ -1,16 +1,13 @@
 import fs from 'fs';
+import { DELAY, MAX_TRIES } from '../constants';
 
-export const checkIfNodeModulesReady = async (
-  dir: string,
-  MAX_TRIES = 20,
-  DELAY = 500
-): Promise<boolean> => {
+export const checkIfNodeModulesReady = async (dir: string): Promise<boolean> => {
   for (let i = 0; i < MAX_TRIES; i++) {
     try {
-      const items = fs.readdirSync(dir).filter((name) => !name.startsWith('.'));
-      if (items.length > 0) return true;
-    } catch (e) {
-      console.log(e);
+      const folders = fs.readdirSync(dir).filter((name) => !name.startsWith('.'));
+      if (folders.length > 0) return true;
+    } catch (_) {
+      console.log('Error checking node_modules folder!');
     }
     await new Promise((r) => setTimeout(r, DELAY));
   }
