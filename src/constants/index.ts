8261c0dfdcef4708b36840f2b0523a7a4a609139
jonpena <gran_200014@hotmail.com>
2025-06-28 02:58:21 +0200

    refactor: add DELAY and MAX_TRIES constants to improve configuration options

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,3 +4,5 @@ export const MAX_DECORATIONS = 1000;
 export const WS_PORT = 9000;
 export const PROXY_PORT = 3000;
 export const DEFAULT_PORT = 5173;
+export const DELAY = 500;
+export const MAX_TRIES = 20;
