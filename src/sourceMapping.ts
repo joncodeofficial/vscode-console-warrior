c1f251e2189f73ec036a365dc99b4794d0c5f065
jonpena <gran_200014@hotmail.com>
2025-06-29 02:12:57 +0200

    refactor: remove redundant console log for cached sourcemaps

--- src/sourceMapping.ts
+++ src/sourceMapping.ts
@@ -25,7 +25,6 @@ export const sourceMapping = async (consoleData: ConsoleData[], sourceMapCache: 
           tracer = new TraceMap(sourceMapData);
           sourceMapCache.set(cacheKey, tracer);
         } else {
-          // console.log('Cached sourcemap to enhance performance.');
           tracer = sourceMapCache.get(cacheKey)!;
         }
 
