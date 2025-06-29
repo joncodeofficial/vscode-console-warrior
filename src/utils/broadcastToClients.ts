d3785a1a7d4c315bcdfdba4d54fb3540848698b8
jonpena <gran_200014@hotmail.com>
2025-06-29 04:35:29 +0200

    refactor: remove unused broadcastToClients utility function

--- src/utils/broadcastToClients.ts
+++
@@ -1,9 +0,0 @@
-import WebSocket from 'ws';
-
-export function broadcastToClients(wss: WebSocket.Server, message: { type: string }) {
-  for (const client of wss.clients) {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(JSON.stringify(message));
-    }
-  }
-}
