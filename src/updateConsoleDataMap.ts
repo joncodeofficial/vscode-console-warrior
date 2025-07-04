cc4b573f9eeaef2674bc247d137a5dd0eafd7a0c
jonpena <gran_200014@hotmail.com>
2025-07-05 00:05:49 +0200

    refactor: enhance consoleDataMap entry creation and message handling logic

--- src/updateConsoleDataMap.ts
+++ src/updateConsoleDataMap.ts
@@ -19,21 +19,27 @@ export const updateConsoleDataMap = (
     if (!consoleDataMap.has(url)) {
       consoleDataMap.set(url, new Map());
     }
+
     const fileMap = consoleDataMap.get(url)!;
 
     // Ensure a denque exists for the line number
     if (!fileMap.has(key)) {
-      fileMap.set(key, new Denque<string>());
+      fileMap.set(key, {
+        counter: 1,
+        consoleMessages: new Denque<string>([message]),
+      });
+      continue;
     }
+
     const messageDenque = fileMap.get(key);
-
-    // If the denque is empty, skip it
+    // Check if one message is already in the denque
     if (!messageDenque) continue;
-
     // Add the new message to the front of the denque
-    messageDenque.unshift(message);
-
-    // Limit the denque size to 1000 messages
-    if (messageDenque.size() > MAX_DECORATIONS) messageDenque.pop();
+    messageDenque.counter++;
+    messageDenque.consoleMessages.unshift(message);
+    // Limit the denque size to MAX_DECORATIONS
+    if (messageDenque.consoleMessages.size() > MAX_DECORATIONS) {
+      messageDenque.consoleMessages.pop();
+    }
   }
 };
