793149a25052943120b237a9c727060e27f8e11e
jonpena <gran_200014@hotmail.com>
2025-07-05 05:23:46 +0200

    chore: add type exports for ConsoleData, ConsoleDataMap, ServerConnections, and SourceMapCache

---
+++ src/types/index.ts
@@ -0,0 +1,4 @@
+export { ConsoleData } from './consoleData.interface';
+export { ConsoleDataMap } from './consoleDataMap.interface';
+export { ServerConnections } from './serverConnections.interface';
+export { SourceMapCache } from './sourceMapCache.interface';
