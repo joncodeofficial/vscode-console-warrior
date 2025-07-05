9328d8c46183d92bbaf44322c713dea21705f548
jonpena <gran_200014@hotmail.com>
2025-07-05 05:23:52 +0200

    chore: add utility functions exports in index.ts

---
+++ src/utils/index.ts
@@ -0,0 +1,8 @@
+export { checkIfNodeModulesReady } from './checkIfNodeModulesReady';
+export { disposable } from './disposable';
+export { formatString } from './formatString';
+export { getFilename } from './getFilename';
+export { getPortFromUrl } from './getPortFromUrl';
+export { isConsoleLogCorrect } from './isConsoleLogCorrect';
+export { sleep } from './sleep';
+export { truncateString } from './truncateString';
