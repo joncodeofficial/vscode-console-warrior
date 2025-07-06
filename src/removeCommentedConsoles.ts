1b1f6664c114c61b65bff9fc2a09e494cadf3e4b
jonpena <gran_200014@hotmail.com>
2025-07-07 01:39:39 +0200

    fix: rename removeCommentedConsoles to removeDecorations and improve variable naming for clarity

--- src/removeCommentedConsoles.ts
+++ src/removeCommentedConsoles.ts
@@ -1,23 +1,37 @@
 import { ConsoleDataMap } from './types';
 import * as vscode from 'vscode';
 import { isConsoleLogCorrect } from './utils';
+import { isInsideConsoleLog } from './utils/isInsideConsoleLog';
 
-export const removeCommentedConsoles = (
+// Remove commented consoles
+export const removeDecorations = (
   editor: vscode.TextDocumentChangeEvent,
   consoleDataMap: ConsoleDataMap
 ) => {
+  // Get the file path of the current editor
   const filePath = editor.document.uri.fsPath;
 
-  for (const [file, innerMap] of consoleDataMap) {
+  for (const [file, positionsMap] of consoleDataMap) {
     if (filePath.endsWith(file)) {
-      for (const [key] of innerMap) {
-        const lineText = editor.document.lineAt(parseInt(key) - 1).text;
+      for (const [position] of positionsMap) {
+        const line = parseInt(position) - 1;
+        const lineText = editor.document.lineAt(line).text;
 
-        if (!isConsoleLogCorrect(lineText) && innerMap.has(key)) {
-          innerMap.delete(key);
+        // if console.log is commented, delete
+        if (!isConsoleLogCorrect(lineText) && positionsMap.has(position)) {
+          positionsMap.delete(position);
+          continue;
         }
+
+        const change = editor.contentChanges.find((change) => change.range.start.line === line);
+        // if no considence change, continue
+        if (!change) continue;
+
+        const start = change.range.start.character;
+        // if change inside of the console.log, delete
+        if (isInsideConsoleLog(lineText, start)) positionsMap.delete(position);
       }
-      if (innerMap.size === 0) consoleDataMap.delete(file);
+      if (positionsMap.size === 0) consoleDataMap.delete(file);
     }
   }
 };
