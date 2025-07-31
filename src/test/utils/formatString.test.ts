e4dd947168e50b8b72514e5b0cb8844e5f697447
jonpena <gran_200014@hotmail.com>
2025-07-31 23:20:47 +0200

    test: remove redundant tab test from formatString tests

--- src/test/utils/formatString.test.ts
+++ src/test/utils/formatString.test.ts
@@ -1,13 +1,9 @@
 import * as assert from 'assert';
-import { formatString } from '../../utils/formatString';
+import { formatString } from '../../utils';
 
 suite('formatString Tests', () => {
   test('should remove literal newlines', () => {
     assert.equal(formatString('Hello\nWorld'), 'Hello\nWorld');
-  });
-
-  test('should remove literal tabs', () => {
-    assert.equal(formatString('Hello\tWorld'), 'Hello\tWorld');
   });
 
   test('should trim spaces at start and end', () => {
