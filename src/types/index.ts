4333c2853f000d52a0f7818dfcf6b331de014bb2
jonpena <gran_200014@hotmail.com>
2025-08-10 03:13:42 +0200

    refactor: adding types to barren file

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,4 @@
 export { ConsoleData } from './consoleData.types';
-export { ConsoleDataMap, ConsoleDataMapValue } from './consoleDataMap.types';
+export { ConsoleDataMap, ConsoleDataMapValue, ConsoleMessagesType } from './consoleDataMap.types';
 export { ServerConnections } from './serverConnections.types';
 export { SourceMapCache } from './sourceMapCache.types';
