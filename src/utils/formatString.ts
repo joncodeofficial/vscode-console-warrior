64703a16f4f2a7a7a81c2bdddeebcad0ba92041e
jonpena <gran_200014@hotmail.com>
2025-08-18 08:51:18 +0200

    fix: remove all line breaks in string

--- src/utils/formatString.ts
+++ src/utils/formatString.ts
@@ -1,5 +1,6 @@
 export const formatString = (message: string) => {
   return message
     .replace(/^\s+|\s+$/g, '') // Delete spaces at start and end
-    .replace(/\s{2,}/g, ' '); // Replace multiple spaces with one
+    .replace(/\s{2,}/g, ' ') // Replace multiple spaces with one
+    .replace(/\n/g, ' '); // Remove all line breaks
 };
