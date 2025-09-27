bff9b47c82ee3b8b859477e4b72af442ba82ac34
jonpena <gran_200014@hotmail.com>
2025-09-27 14:13:34 +0200

    fix: MAX_DECORATIONS constant is now 500

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,6 @@
 export const MAX_MESSAGE_LENGTH = 150;
 export const UPDATE_RATE = 100;
-export const MAX_DECORATIONS = 1000;
+export const MAX_DECORATIONS = 500;
 export const WS_PORT = 27020;
 export const PROXY_PORT = 3000;
 export const DEFAULT_PORT = 5173;
